14c12404726008520ea21c8fa12757a0c4e75dc0
Jason Borneman <jborneman@jborneman-ltm1.internal.salesforce.com>
2021-01-18 23:09:38 -0500

    still trying to fix stupid auth

--- function.go
+++ function.go
@@ -10,28 +10,29 @@ import (
 	s3 "github.com/jasondborneman/solar3/Solar3App"
 )
 
-type StupidAuth struct {
-	stupidAuth string
-}
-
 func Solar3(w http.ResponseWriter, r *http.Request) {
-	var sa StupidAuth
-	err := json.NewDecoder(r.Body).Decode(&sa)
+	var d struct {
+		StupidAuth string `json:"stupidAuth"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&d)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	stupidAuthLocal := os.Getenv("STUPID_AUTH")
-	if sa.stupidAuth == stupidAuthLocal {
+	fmt.Println(d)
+	if d.StupidAuth == stupidAuthLocal {
 		doTweet := os.Getenv("DO_TWEET") == "true"
 		doSaveGraph := os.Getenv("DO_SAVEGRAPH") == "true"
 		s3.Run(doTweet, doSaveGraph, false)
+		fmt.Fprint(w, "Success")
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
 		bodyBytes, _ := ioutil.ReadAll(r.Body)
 		bodyString := string(bodyBytes)
-		fmt.Printf("Unauthorized access attempt. [%s | %s]\n", sa.stupidAuth, stupidAuthLocal)
+		fmt.Printf("Unauthorized access attempt. [%s | %s]\n", d.StupidAuth, stupidAuthLocal)
+		fmt.Printf("%s", bodyString)
 		returnMessage := fmt.Sprintf("Unauthorized. [%s]", bodyString)
 		fmt.Fprint(w, returnMessage)
 	}
